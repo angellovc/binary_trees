dea3e3d66a3658223083b9ef5ceef381c55cbc09
angellovc <vc.angel@outlook.com>
2020-07-21 19:34:00 +0000

    Feat fix 9-binary_tree_height.c
    Direction parameter was deleted, due to it was
    unnecesary

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,19 +6,16 @@
  * right child and 0 in left child
  * Return: height of the binary tree
  */
-size_t find_height_binary(const binary_tree_t *tree, int direction)
+size_t find_height_binary(const binary_tree_t *tree)
 {
 	size_t h_left = 0, h_right = 0;
 
 	if (tree == NULL)
-		return (0);
-
-	h_left = find_height_binary(tree->left, 0);
-	h_right = find_height_binary(tree->right, 1);
-	if (direction == 0)
-		return (h_left + 1);
-	if (direction == 1)
-		return (h_right + 1);
+		return (-1);
+	h_left = find_height_binary(tree->left);
+	h_right = find_height_binary(tree->right);
+	h_left += 1;
+	h_right += 1;
 	if (h_left > h_right)
 		return (h_left);
 	return (h_right);
@@ -31,5 +28,5 @@ size_t find_height_binary(const binary_tree_t *tree, int direction)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	return (find_height_binary(tree, 2));
+	return (find_height_binary(tree));
 }
