38464c0fa6fec503a0ea568ed83c1976e67c10ff
angellovc <vc.angel@outlook.com>
2020-07-21 16:32:53 +0000

    New file:   6-binary_tree_preorder.c
    New feat: binary_tree_preorder function

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_preorder - apply a function to each node of the binary tree
+ * using the preorder traversal way
+ * @tree: binary tree
+ * @func: function to apply to
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL)
+		return;
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+}
