028532afab37db0024080beca7ee58b7a1e45e82
angellovc <vc.angel@outlook.com>
2020-07-21 16:15:06 +0000

    Feat fix: feat fix at  1-binary_tree_insert_left.c
    The style was modified to improve the readability

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,5 +1,11 @@
 #include "binary_trees.h"
-
+/**
+ * binary_tree_insert_left - insert a new binary tree node at the left
+ * as a child of the parent node
+ * @parent: parent node
+ * @value: value of the new node
+ * Return: pointer to a new node
+ */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node, *tmp;
