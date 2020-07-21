3d40fa0af6dd982bd9885d562ec6f09d2ed93059
angellovc <vc.angel@outlook.com>
2020-07-21 23:54:59 +0000

    New file:   10-binary_tree_depth.c New feat: binary_tree_depth

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_depth - find a depth of a binary tree's node
+ * @tree: binary tree
+ * Return: depth
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t depth = 0;
+
+	if (tree->parent == NULL)
+		return (0);
+	depth = 1 + binary_tree_depth(tree->parent);
+	return (depth);
+}
