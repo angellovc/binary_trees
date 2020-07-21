1e093b78c53ae7167490d95b942749fba880c844
angellovc <vc.angel@outlook.com>
2020-07-21 16:37:07 +0000

    New file:   8-binary_tree_postorder.c New feat: binary_tree_postorder function was added

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_postorder - apply a function to each node of the binary tree
+ * using the postorder traversal way
+ * @tree: binary tree
+ * @func: function to apply to
+ */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL)
+		return;
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
+	func(tree->n);
+}
