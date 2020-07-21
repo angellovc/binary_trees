951d7209495a051a3c911010a3e053b50a8fe0ff
angellovc <vc.angel@outlook.com>
2020-07-21 19:15:21 +0000

    Feat fix at 2-binary_tree_insert_right.c
    Condition at line 18 was changed to parent->right
    to add the new node in the correct place

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -15,7 +15,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	node = binary_tree_node(parent, value);
 	if (node == NULL)
 		return (NULL);
-	if (parent->left)
+	if (parent->right)
 	{
 		tmp = parent->right;
 		tmp->parent = node;
