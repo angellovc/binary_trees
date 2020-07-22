15978d3928ab7aaf8583579520ba393d3bee291e
angellovc <vc.angel@outlook.com>
2020-07-22 00:19:59 +0000

    Feat fix at 10-binary_tree_depth.c
    New condition added at 11 line to avoid overflows

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -8,7 +8,7 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	size_t depth = 0;
 
-	if (tree->parent == NULL)
+	if (tree == NULL || tree->parent == NULL)
 		return (0);
 	depth = 1 + binary_tree_depth(tree->parent);
 	return (depth);
