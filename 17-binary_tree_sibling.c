0dc134a83ac358edd32c5bb714eb984bf8da9bbe
angellovc <vc.angel@outlook.com>
2020-07-22 22:13:30 +0000

    Feat fix:   17-binary_tree_sibling.c
    New condition added at the begining of the function to avoid overflows

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -6,8 +6,11 @@
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	binary_tree_t *parent_node = node->parent;
+	binary_tree_t *parent_node;
 
+	if (!node)
+		return(NULL);
+	parent_node = node->parent;
 	if (!parent_node)
 		return (NULL);
 	if (!parent_node->left || !parent_node->right)
