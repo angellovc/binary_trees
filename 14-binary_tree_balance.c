21f4dc8bd1243b74141f905f3b2ba79860be5a4f
angellovc <vc.angel@outlook.com>
2020-07-22 16:49:11 +0000

    Feat fix at 14-binary_tree_balance.c
    Parameter direcion was changed to first_node
    recursion condition was modified
    Now first_node represent the moment in which the function
    must return the balance factor of the binary tree

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,23 +1,24 @@
+
 #include "binary_trees.h"
 /**
  * balance_difference -  measures the balance factor of a binary tree
  * @tree: binary tree
- * @direction: recursion direction
+ * @first_node: represent the first node
  * Return: the balance factor
  */
-int balance_difference(const binary_tree_t *tree, int direction)
+int balance_difference(const binary_tree_t *tree, int first_node)
 {
 	int l = 0, r = 0;
 
-	if (!tree)
-		return (0);
-	l = balance_difference(tree->left, 0);
-	r = balance_difference(tree->right, 1);
-	if (direction == 0)
-		return (l + 1);
-	if (direction == 1)
-		return (r + 1);
-	return (l - r);
+	if (tree->left)
+		l = 1 + balance_difference(tree->left, 0);
+	if (tree->right)
+		r = 1 + balance_difference(tree->right, 0);
+	if (first_node)
+		return (l - r);
+	if (l > r)
+		return (l);
+	return (r);
 }
 /**
  * binary_tree_balance - prepare the terrain to call balance_difference
@@ -29,5 +30,5 @@ int binary_tree_balance(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	return (balance_difference(tree, 2));
+	return (balance_difference(tree, 1));
 }
