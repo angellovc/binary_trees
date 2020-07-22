4b114708e0672b3d75d4a37a902ebbcbeb4c00c1
angellovc <vc.angel@outlook.com>
2020-07-22 02:43:58 +0000

    New file:   13-binary_tree_nodes.c New feat: binary_tree_nodes Update: binary_trees.h
    Prototype of binary_tree_nodes was added into the header

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_nodes - counts the nodes with at least 1 child in a binary tree
+ * @tree: binary tree
+ * Return: 0 if tree is NULL or number of fathers
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t father = 1;
+
+	if (tree == NULL)
+		return (0);
+	if (!tree->left && !tree->right)
+		father = 0;
+	return (binary_tree_nodes(tree->left) +
+	binary_tree_nodes(tree->right) + father);
+}
